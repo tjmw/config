5b91232bbac8ceb436a4c2d54893d82ef44b5d14
Tom Wey <tjmwey@gmail.com>
2026-09-14 12:12:19 +0100

    Add aliases and link in install script

---
+++ .bash_aliases
@@ -0,0 +1,6 @@
+alias g='git'
+alias gg='git grep'
+alias gst='git status'
+alias gco='git checkout'
+alias pf='pnpm --filter'
+
--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Running install.sh"
+
+ALIASES_TARGET="$HOME/config/.bash_aliases"
+ALIASES_LINK="$HOME/.bash_aliases"
+
+if [ ! -L "$ALIASES_LINK" ]; then
+  ln -s "$ALIASES_TARGET" "$ALIASES_LINK"
+  echo "Created symlink: $ALIASES_LINK -> $ALIASES_TARGET"
+fi
