3b4c4f448347d9434086c2348d1cb2266a6aa594
Tom Wey <tjmwey@gmail.com>
2026-09-18 09:27:17 +0100

    Git autocompletion

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -12,3 +12,13 @@ fi
 
 sudo apt -y update
 sudo apt install fzf
+
+add_to_bashrc() {
+    local line="$1"
+    grep -qxF -- "$line" "$HOME/.bashrc" || echo "$line" >> "$HOME/.bashrc"
+}
+
+# Setup git autocompletions
+add_to_bashrc 'source /usr/share/bash-completion/completions/git'
+# Make it work with the g alias
+add_to_bashrc '__git_complete g __git_main'
